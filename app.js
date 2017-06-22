ddc35c84b810597ded12d63c9fbda9c088719691
Isaiah Walker <Walkisai.72@gmail.com>
2017-06-22 15:27:38 -0700

    event listener and handler and local storage

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 
 var products = ['select', 'bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'metro', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
+var allOrders = [];
+
 var selectEl = document.getElementById('dropbox');
-var buyButton = document.getElementById('buy');
+var form = document.getElementById('form');
 
 for (var i = 0; i < products.length; i++) {
   var optionEl = document.createElement('option');
@@ -21,14 +23,28 @@ function Order(product, quantity, name, street, city, state, zip, digits, ccard)
   this.zip = zip;
   this.digits = digits;
   this.ccard = ccard;
+  allOrders.push(this);
 }
 
-Order.prototype.createLS = function () {
-  localStorage.data = JSON.stringify(this);
-};
+if(localStorage.lsOrders) {
+  allOrders = JSON.parse(localStorage.lsOrders);
+}
 
-buyButton.addEventListener('submit', handleSubmit);
+form.addEventListener('submit', handleSubmit);
 
-function handleSubmit() {
-  
+function handleSubmit(event) {
+  console.log('test2');
+  event.preventDefault();
+  var product = event.target.product.value;
+  var quant = event.target.quant.value;
+  var userName = event.target.userName.value;
+  var street = event.target.street.value;
+  var city = event.target.city.value;
+  var state = event.target.state.value;
+  var zip = event.target.zip.value;
+  var digits = event.target.digits.value;
+  var ccard = event.target.ccard.value;
+
+  new Order(product, quant, userName, street, city, state, zip, digits, ccard);
+  localStorage.lsOrders = JSON.stringify(allOrders);
 }
